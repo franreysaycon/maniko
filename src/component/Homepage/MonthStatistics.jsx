84b1d44d0d85e81ff37841afa7d85da68c20f9f4
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-04-01 19:17:36 +0800

    Realign statistics

--- src/component/Homepage/MonthStatistics.jsx
+++ src/component/Homepage/MonthStatistics.jsx
@@ -7,19 +7,19 @@ const MonthStatistics = ({
   <Box h="20%" d="flex" flexDir="column" alignItems="stretch">
     <Text color="white">SUMMARY</Text>
     <Box d="flex" h="100%" flex="1">
-      <Box flex="1" h="100%" bgColor="red.200" d="flex" alignItems="center" justifyContent="center" borderLeftRadius="15px" flexDir="column" lineHeight="1">
-        <Text color="white" fontSize="xl" fontWeight="bold">{`PHP ${after30thSalary}`}</Text>
-        <Text color="white" fontSize="xs" mb="10px">AFTER 30TH</Text>
-        <Text color="white" fontSize="xs" alignSelf="flex-end" mr="5px" pos="relative" top="10px">
+      <Box pos="relative" flex="1" h="100%" bgColor="red.200" d="flex" alignItems="center" justifyContent="center" borderLeftRadius="15px" flexDir="column" lineHeight="1">
+        <Text color="white" fontSize="xs" pos="absolute" top="10px" left="10px">
           {`PHP ${original30th}`}
         </Text>
+        <Text color="white" fontSize="xl" fontWeight="bold" mt="20px">{`PHP ${after30thSalary}`}</Text>
+        <Text color="white" fontSize="xs" mb="10px">AFTER 30TH</Text>
       </Box>
-      <Box flex="1" h="100%" bgColor="orange.100" d="flex" alignItems="center" justifyContent="center" borderRightRadius="15px" flexDir="column" lineHeight="1">
-        <Text color="white" fontSize="xl" fontWeight="bold">{`PHP ${after15thSalary}`}</Text>
-        <Text color="white" fontSize="xs" mb="10px">AFTER 15TH</Text>
-        <Text color="white" fontSize="xs" alignSelf="flex-end" mr="5px" pos="relative" top="10px">
+      <Box pos="relative" flex="1" h="100%" bgColor="orange.100" d="flex" alignItems="center" justifyContent="center" borderRightRadius="15px" flexDir="column" lineHeight="1">
+        <Text color="white" fontSize="xs" pos="absolute" top="10px" left="10px">
           {`PHP ${original15th}`}
         </Text>
+        <Text color="white" fontSize="xl" fontWeight="bold" mt="20px">{`PHP ${after15thSalary}`}</Text>
+        <Text color="white" fontSize="xs" mb="10px">AFTER 15TH</Text>
       </Box>
     </Box>
   </Box>
